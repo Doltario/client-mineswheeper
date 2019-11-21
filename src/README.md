b840143ed61e945a8fa0545b4395e42518686bba
felix <felix@beyowi.com>
2019-11-21 21:22:46 +0100

    Add basic structure + One request to custom API

---
+++ src/README.md
@@ -0,0 +1,13 @@
+# Installing project
+
+```
+$ yarn install
+```
+
+# Launching project
+
+```
+$ npm run serve
+```
+
+Project should be launch at http://localhost:8080
--- src/main.js
+++ src/main.js
@@ -1,8 +1,23 @@
 import Vue from 'vue'
 import App from './App.vue'
 
+import VueRouter from 'vue-router'
+
+import HelloWorld from './components/HelloWorld.vue'
+import Users from './components/Users.vue'
+
+Vue.use(VueRouter)
+
 Vue.config.productionTip = false
 
+const router = new VueRouter({
+  routes: [
+    { path: '/', component: HelloWorld },
+    { path: '/users', component: Users }
+  ]
+})
+
 new Vue({
-  render: h => h(App),
+  router,
+  render: h => h(App)
 }).$mount('#app')

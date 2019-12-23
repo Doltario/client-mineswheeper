56642064291518d7a451ad1beeead6ac6ade2fdc
Félix Darraud <felix.darraud@gmail.com>
2019-12-23 22:17:17 +0100

    Added view to monitor grid and neighbors computing

---
+++ src/components/Grid.vue
@@ -0,0 +1,45 @@
+<template>
+  <!-- A template element accept only ONE root container == One div must contain everything -->
+  <div class="grid-container">
+    <div class="grid-content" :style="`width: ${20 * gameGrid.width }px`">
+      <div v-for="gridCase in gameGrid.grid" v-bind:key="gridCase.id" class="case">
+        <div v-if="gridCase.hasBomb === true">
+          T
+        </div>
+      </div>
+    </div>
+    <pre>{{ gameGrid }}</pre>  
+  </div>
+</template>
+
+<script>
+export default {
+  props: ["gameGrid"]
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss">
+  .grid-container {
+    width: 100%;
+    height: 100%;
+
+    .grid-content {
+      width: 100%;
+      height: 100%;
+      display: flex;
+      flex-wrap: wrap;
+
+      .case {
+        height: 20px;
+        width: 20px;
+        background-color: lightgrey;
+        border: 1px solid black;
+        display: flex;
+        justify-content: center;
+        align-content: center;
+        cursor: pointer;
+      }
+    }
+  }
+</style>
--- src/services/MinesWheeper.js
+++ src/services/MinesWheeper.js
@@ -34,7 +34,7 @@ class MinesWheeper {
   }
 
   _writeCase(hasBomb) {
-    const caseItem = { revealed: false, hasBomb: hasBomb }
+    const caseItem = { revealed: false, hasBomb: hasBomb, index: null }
     this._grid.push(caseItem)
     return this
   }
@@ -53,12 +53,88 @@ class MinesWheeper {
     return this
   }
 
+  _assignIndexes() {
+    this._grid.forEach((caseItem, index) => {
+      caseItem.index = index
+    })
+  }
+
+  _computeNeighbors() { // felix@TODO: This is problably not the best way, but this is the simplest way I see for now
+    this._grid.forEach((caseItem) => {
+      caseItem.neighbors = new Array(8)
+      // 0 1 2
+      // 3 X 4
+      // 5 6 7
+
+      caseItem.neighbors[0] = caseItem.index - this._width -1
+      caseItem.neighbors[1] = caseItem.index - this._width
+      caseItem.neighbors[2] = caseItem.index - this._width + 1
+      caseItem.neighbors[3] = caseItem.index - 1
+      caseItem.neighbors[4] = caseItem.index + 1
+      caseItem.neighbors[5] = caseItem.index + this._width - 1
+      caseItem.neighbors[6] = caseItem.index + this._width
+      caseItem.neighbors[7] = caseItem.index + this._width + 1
+
+      if (caseItem.index % this._width === 0) { // Is left
+        caseItem.neighbors[0] = null
+        caseItem.neighbors[3] = null
+        caseItem.neighbors[5] = null
+
+      }
+      if (caseItem.index % this._width === this._width - 1) { // Is right
+        caseItem.neighbors[2] = null
+        caseItem.neighbors[4] = null
+        caseItem.neighbors[7] = null
+      }
+      if (caseItem.index > (this._grid.length - this._width - 1)) { // Is bottom
+        caseItem.neighbors[5] = null
+        caseItem.neighbors[6] = null
+        caseItem.neighbors[7] = null
+      }
+      if (caseItem.index <= this._width) { // Is top
+        caseItem.neighbors[0] = null
+        caseItem.neighbors[1] = null
+        caseItem.neighbors[2] = null
+      }
+      
+      caseItem.neighbors = caseItem.neighbors.filter((neigborIndex) => {
+        return neigborIndex !== null
+      })
+      return this
+    })
+  }
+
+  findCase(caseIndex) {
+    if (!caseIndex || typeof caseIndex !== 'number') {
+      throw new Error (`First parameter of findCase() must be a number, ${typeof caseIndex} given`)
+    }
+
+    return this._grid.filter((caseItem) => {
+      return caseItem.index === caseIndex
+    })[0]
+  }
+
   generateGrid() {
     for (let i = 0; i < this._casesNumber; i++) {
       this.writeCase()
     }
 
     this.fillGrid()
+
+    this._assignIndexes()
+    this._computeNeighbors()
+  }
+
+  get grid () {
+    return this._grid
+  }
+
+  get height () {
+    return this.height
+  }
+
+  get width () {
+    return this._width
   }
 }
 

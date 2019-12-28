6fa85ffb6a461d95aea8b0da9df744e433138975
Félix Darraud <felix.darraud@gmail.com>
2019-12-28 12:57:06 +0100

    Try using Object.defineProperty instead of Object.freeze

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -12,7 +12,15 @@
     </ul>
     <div>
       <h2>This is a grid</h2>
-      <Grid :grid="game.grid" />
+      <div>
+        <p>{{ a }}</p>
+        <button @click="a++">a++</button>
+        <p>{{ b }}</p>
+        <button @click="b++">b++</button>
+        <p>{{ c }}</p>
+        <button @click="c++">c++</button>
+      </div>
+      <Grid :grid="$data.game.grid" />
     </div>
   </div>
 </template>
@@ -22,16 +30,47 @@ import { MinesWheeper } from '@services/MinesWheeper.js'
 
 import Grid from '@components/Grid.vue'
 
+function defineNonReactiveProperties(target, properties) {
+  for (const k in properties) {
+    const value = properties[k]
+    const opts = {
+      configurable: false
+      // enumerable: true
+    }
+    if (value !== null && (typeof value.get === 'function' || typeof value.set === 'function')) {
+      opts.get = value.get
+      opts.set = value.set
+    } else {
+      opts.writable = true
+      opts.value = value
+    }
+    Object.defineProperty(target, k, opts)
+  }
+}
+
+const nonReactiveData = {
+  a: 0
+}
+
 export default {
   components: {
     Grid
   },
   data() {
-    const game = new MinesWheeper(30, 30, 40)
-    Object.freeze(game)
-    return {
-      game
-    }
+    defineNonReactiveProperties(nonReactiveData, {
+      game: new MinesWheeper(30, 30, 40),
+      b: 0,
+      c: {
+        get() {
+          return this.b * 2
+        },
+        set() {
+          this.a += 2
+        }
+      }
+    })
+
+    return nonReactiveData
   }
 }
 </script>

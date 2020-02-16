62565f2b9b4d908d6dd1192a8ec2ef57846909f3
Félix Darraud <felix.darraud@gmail.com>
2020-02-16 11:58:40 +0100

    Try to improve reactivy on box reveal
    It is better but not enough

--- src/services/MinesWheeperClient.js
+++ src/services/MinesWheeperClient.js
@@ -2,7 +2,32 @@ const createGame = async () => {
   try {
     const response = await fetch('http://localhost:3001/game')
     const jsonResponse = await response.json()
-    return jsonResponse
+
+    const game = {}
+
+    const opts = {
+      configurable: false,
+      enumerable: true,
+      writable: true
+    }
+    Object.defineProperty(game, 'grid', {
+      ...opts,
+      value: jsonResponse.grid ? jsonResponse.grid : {}
+    })
+
+    // const neighbors = jsonResponse.neighbors
+    // delete jsonResponse.neighbors
+    // const opts = {
+    //   configurable: false,
+    //   enumerable: true,
+    //   writable: true
+    // }
+    // Object.defineProperty(jsonResponse, 'neighbors', {
+    //   //TODO: remove
+    //   ...opts,
+    //   value: jsonResponse.neighbors ? jsonResponse.neighbors.concat(neighbors) : neighbors
+    // })
+    return game
   } catch (error) {
     console.error(`Cannot fetch: ${error}`)
   }

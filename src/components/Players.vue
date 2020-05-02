98feef4f67d98e063bf137a8d077c0ad1c8cfe86
Félix Darraud <felix.darraud@gmail.com>
2020-05-02 11:56:08 +0200

    Add few style on cursors

--- src/components/Players.vue
+++ src/components/Players.vue
@@ -6,7 +6,10 @@
       <li :key="player.socketId" v-for="player in $store.state.players.others">
         {{ player.nickname }}
         <span v-if="player.socketId === $store.state.players.me.socketId">(You)</span>
-        <div v-if="player.socketId !== $store.state.players.me.socketId" class="player-cursor" :style="{ top: player.position.y + 'px', left: player.position.x + 'px' }">V</div>
+        <div v-if="player.socketId !== $store.state.players.me.socketId" class="player-cursor-container" :style="{ top: player.position.y + 'px', left: player.position.x + 'px' }">
+          <span class="cursor"></span>
+          <span class="name">{{ player.nickname }}</span>
+        </div>
       </li>
     </ul>
   </div>
@@ -31,8 +34,26 @@ export default {
   width: 100%;
   height: 100%;
 
-  .player-cursor {
+  .player-cursor-container {
     position: absolute;
+    pointer-events: none;
+
+    .cursor {
+      display: block;
+      width: 0;
+      height: 0;
+      border-left: 5px solid transparent;
+      border-right: 5px solid transparent;
+      border-bottom: 12px solid purple;
+      transform: rotate(-45deg);
+    }
+
+    .name {
+      position: absolute;
+      top: 75%;
+      left: 125%;
+      font-size: 10px;
+    }
   }
 }
 </style>
